5248d3f32f89f7bba27831e462190c027bde868a
axl <axl.castilloblasco@gmail.com>
2025-06-27 17:38:28 -0400

    about page setup

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,11 +1,34 @@
-import { useState, useEffect } from "react";
+import Time from "../components/Time";
 import "../about.css";
 
 function AboutPage() {
 	return (
-		<div>
-			<h1>About Us</h1>
-			<p>This is the about page of our application.</p>
+		<div className="about-page">
+			<div className="center">
+				<div className="info-container">
+					<img src="./img/pfp.jpeg" alt="profile_picture" className="pfp" />
+					<div className="information">
+						<p>hello@irvingmarca.com</p>
+						<p>+34 652 697 491</p>
+						<a href="http://" target="_blank" rel="noopener noreferrer">
+							Instagram
+						</a>
+						<a href="http://" target="_blank" rel="noopener noreferrer">
+							Vimeo
+						</a>
+						<br />
+						<p>1/2 _________Límite</p>
+					</div>
+				</div>
+			</div>
+			<div className="bottom-left corner">
+				IRVING MARCA
+				<br />
+				COLORIST & DIRECTOR
+			</div>
+			<div className="top-right corner">
+				<Time />
+			</div>
 		</div>
 	);
 }
